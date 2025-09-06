1f189bb4be4a144183eb36fb4fdfbbedb96dca1d
Usman Ghani <usman3123@gmail.com>
2025-09-06 21:25:11 +0500

    fix: use process.env.PORT for Railway

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.post("/save-score", (req, res) => {
   res.json({ message: "Score save ho gaya ✅", newScore });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
